0298cf8ad505a96834587470dc6e337d154321b2
alexd106 <alexd106@gmail.com>
2019-11-28 11:31:56 +0000

    incremental reveal

--- reveal.R
+++ reveal.R
@@ -1,5 +1,7 @@
 # Change to TRUE to show exercise solutions and R scripts
-show_text <- FALSE
+show_text12 <- FALSE
+show_text34 <- FALSE
+show_text56 <- FALSE
 
 # Change to TRUE to show lectures
 show_lecture <- FALSE
