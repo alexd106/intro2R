4f7c2c303d6d83c5b1a93f3480e338c24e5dc9e2
alexd106 <alexd106@gmail.com>
2019-11-28 12:03:27 +0000

    added comments

--- reveal.R
+++ reveal.R
@@ -1,7 +1,7 @@
 # Change to TRUE to show exercise solutions and R scripts
-show_text12 <- FALSE
-show_text34 <- FALSE
-show_text56 <- FALSE
+show_text12 <- FALSE    # ex 1 and 2
+show_text34 <- FALSE    # ex 3 and 4
+show_text56 <- FALSE    # ex 5 and 6
 
 # Change to TRUE to show lectures
 show_lecture <- FALSE
