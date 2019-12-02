131ce35b56d1bcbacebdfa2ab02cc968d2bbb48d
alexd106 <alexd106@gmail.com>
2019-12-02 09:10:51 +0000

    reveal lecture

--- reveal.R
+++ reveal.R
@@ -4,7 +4,7 @@ show_text34 <- FALSE    # ex 3 and 4
 show_text56 <- FALSE    # ex 5 and 6
 
 # Change to TRUE to show lectures
-show_lecture <- FALSE
+show_lecture <- TRUE
 
 # Change to TRUE to show exercises
 show_exercise <- TRUE
