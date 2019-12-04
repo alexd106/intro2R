02794563185d0d60feab064fb760155bb6846c83
alexd106 <alexd106@gmail.com>
2019-12-04 08:27:34 +0000

    reveal sol34

--- reveal.R
+++ reveal.R
@@ -1,6 +1,6 @@
 # Change to TRUE to show exercise solutions and R scripts
 show_text12 <- TRUE    # ex 1 and 2
-show_text34 <- FALSE    # ex 3 and 4
+show_text34 <- TRUE    # ex 3 and 4
 show_text56 <- FALSE    # ex 5 and 6
 
 # Change to TRUE to show lectures
