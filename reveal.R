6e3210d5043651cb6a8d30613c70597610cef590
alexd106 <alexd106@gmail.com>
2019-12-05 08:20:57 +0000

    reveal sol56

--- reveal.R
+++ reveal.R
@@ -1,7 +1,7 @@
 # Change to TRUE to show exercise solutions and R scripts
 show_text12 <- TRUE    # ex 1 and 2
 show_text34 <- TRUE    # ex 3 and 4
-show_text56 <- FALSE    # ex 5 and 6
+show_text56 <- TRUE    # ex 5 and 6
 
 # Change to TRUE to show lectures
 show_lecture <- TRUE
